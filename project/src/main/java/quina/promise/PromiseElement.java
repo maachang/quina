784a3fc45cbc44caac8690d48ee720fb188b6494
maachang <maachang@gmail.com>
2021-03-23 16:33:08 +0900

    Promise修正

---
+++ project/src/main/java/quina/promise/PromiseElement.java
@@ -0,0 +1,37 @@
+package quina.promise;
+
+/**
+ * Promise要素.
+ *
+ * Promiseのステータスと返却Value情報を持ちます.
+ */
+public class PromiseElement {
+	private PromiseStatus status;
+	private Object value;
+
+	/**
+	 * コンストラクタ.
+	 * @param status ステータスを設定します.
+	 * @param value Valueを設定します.
+	 */
+	protected PromiseElement(PromiseStatus status, Object value) {
+		this.status = status;
+		this.value = value;
+	}
+
+	/**
+	 * ステータスを取得.
+	 * @return PromiseStatus ステータスが返却されます.
+	 */
+	public PromiseStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * Valueを取得.
+	 * @return Object Valueが返却されます.
+	 */
+	public Object getValue() {
+		return value;
+	}
+}
