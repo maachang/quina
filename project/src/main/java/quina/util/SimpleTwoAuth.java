d7513a348937ef225d6b98bbbc77ee0b2682956a
maachang <maachang@gmail.com>
2022-04-21 22:09:05 +0900

    SimpleTwoAuthにオブジェクト名を変更.

--- project/src/main/java/quina/util/SimpleTwoAuth.java
+++ project/src/main/java/quina/util/SimpleTwoAuth.java
@@ -5,23 +5,58 @@ import quina.exception.QuinaException;
 /**
  * 単純二段階認証用コードの発行.
  */
-public final class GenerateSimpleTwoAuth {
-	private GenerateSimpleTwoAuth() {}
+public final class SimpleTwoAuth {
+	private SimpleTwoAuth() {}
 	
 	/**
 	 * 二段階認証用のコード作成.
+	 * @param outNextChangeMsec 次の更新時間(ミリ秒)が返却されます.
+	 *                          [0] 次の更新時間.
+	 *                          [1] 最大更新時間.
+	 * @param user 生成識別となるユーザー名を設定します.
+	 * @param domain 生成識別となるドメイン名を設定します.
 	 * @param codeLen コード数(数字桁数)を設定します.
 	 * @param updateTime 生成更新されるタイミングを設定します.
 	 *                   設定値は秒で設定します.
-	 * @param user 生成識別となるユーザー名を設定します.
-	 * @param domain 生成識別となるドメイン名を設定します.
 	 * @return String[] 有効な二段階認証コードが返却されます.
 	 *                  [0]は、生成更新タイミングより１つ前のコードです.
 	 *                  [1]は、生成更新タイミングのコードです.
 	 *                  [2]は、生成更新タイミングより１つ後のコードです.
+	 *                  入力結果をチェックする場合は[0]か[1]で判別すれば
+	 *                  良いです.
 	 */
 	public static final String[] create(
-		int codeLen, int updateTime, String user, String domain) {
+		int[] outNextChangeMsec,
+		String user, String domain,
+		int codeLen, int updateTime) {
+		return create(
+			outNextChangeMsec, user, domain,
+			codeLen, updateTime, null);
+	}
+	/**
+	 * 二段階認証用のコード作成.
+	 * @param outNextChangeMsec 次の更新時間(ミリ秒)が返却されます.
+	 *                          [0] 次の更新時間.
+	 *                          [1] 最大更新時間.
+	 * @param user 生成識別となるユーザー名を設定します.
+	 * @param domain 生成識別となるドメイン名を設定します.
+	 * @param codeLen コード数(数字桁数)を設定します.
+	 * @param updateTime 生成更新されるタイミングを設定します.
+	 *                   設定値は秒で設定します.
+	 * @param cu カスタムでUserとDomainをLong変換する処理を設定します.
+	 * @return String[] 有効な二段階認証コードが返却されます.
+	 *                  [0]は、生成更新タイミングより１つ前のコードです.
+	 *                  [1]は、生成更新タイミングのコードです.
+	 *                  [2]は、生成更新タイミングより１つ後のコードです.
+	 *                  入力結果をチェックする場合は[0]か[1]で判別すれば
+	 *                  良いです.
+	 */
+	public static final String[] create(
+		int[] outNextChangeMsec,
+		String user, String domain,
+		int codeLen, int updateTime,
+		CustomUserDomain cu) {
+		// 引数チェック.
 		if(codeLen <= 0) {
 			throw new QuinaException(
 				"The number of number frames is 0 or less.");
@@ -34,8 +69,34 @@ public final class GenerateSimpleTwoAuth {
 			throw new QuinaException(
 				"The target domain name has not been set.");
 		}
-		long now = now(updateTime);
-		long userDomainCode = userDomainByLong(user, domain);
+		// updateTimeに対する現在時間の値を取得.
+		long now = nowTiming(updateTime);
+		// 更新残り時間を取得.
+		if(outNextChangeMsec != null &&
+			outNextChangeMsec.length >= 1) {
+			// 残り時間をセット.
+			outNextChangeMsec[0] = (int)(
+				(updateTime * 1000L) -
+				(
+					(System.currentTimeMillis()) -
+					(now * 1000L)
+				)
+			);
+			// 最大時間をセット.
+			if(outNextChangeMsec.length >= 2) {
+				outNextChangeMsec[1] = updateTime * 1000;
+			}
+		}
+		// ユーザーとドメインのlong化.
+		long userDomainCode;
+		if(cu != null) {
+			/// カスタム変換.
+			userDomainCode = cu.convert(user, domain);
+		} else {
+			// 既存変換
+			userDomainCode = userDomainByLong(user, domain);
+		}
+		// ２段階認証コードを取得.
 		return new String[] {
 			createTowAuthCode(codeLen, now - updateTime, userDomainCode)
 			,createTowAuthCode(codeLen, now, userDomainCode)
@@ -43,12 +104,11 @@ public final class GenerateSimpleTwoAuth {
 		};
 	}
 	
-	// 現在時間を取得.
-	private static final long now(int updateTime) {
+	// updateTimeに対する現在時間の値を取得.
+	private static final long nowTiming(int updateTime) {
 		long utime = (long)updateTime;
 		return (long)((double)(System.currentTimeMillis() / 1000L) /
 			(double)utime) * utime;
-
 	}
 	
 	// user + domain をlong変換.
@@ -114,11 +174,14 @@ public final class GenerateSimpleTwoAuth {
 		} else {
 			len = codeLen;
 		}
+		// 認証コードを生成(2文字)
 		for(int i = 0; i < len; i +=2) {
 			ret.append((n = r.next() & 0x7fffffff) % 10);
 			ret.append((n / 100) % 10);
 		}
+		// 残りの認証コード生成が必要な場合.
 		if((codeLen & 0x01) != 0) {
+			// 認証コードを生成(1文字)
 			ret.append((r.next() & 0x7fffffff) % 10);
 		}
 		return ret.toString();
@@ -165,16 +228,34 @@ public final class GenerateSimpleTwoAuth {
 		}
 	}
 	
-	/*
+	/**
+	 * ユーザー・ドメインをlong変換.
+	 */
+	public static interface CustomUserDomain {
+		/**
+		 * 変換処理.
+		 * @param user 生成識別となるユーザー名を設定します.
+		 * @param domain 生成識別となるドメイン名を設定します.
+		 * @return long 値が返却されます.
+		 */
+		public long convert(String user, String domain);
+	}
+	
+	// test.
+	/**
 	public static final void main(String[] args) {
 		int codeLen = 6;
-		int time = 5;
-		String[] code1 = create(
-			codeLen, time, "maachang@hogehoge.jp", "hogehoge.jp");
-		String[] code2 = create(
-			codeLen, time, "maachang@mogemoge.com", "mogemoge.com");
+		int time = 30;
+		int[] out = new int[2];
+		String[] code1 = create(out,
+			"maachang@hogehoge.jp", "hogehoge.jp", codeLen, time);
+		String[] code2 = create(out,
+			"maachang@mogemoge.com", "mogemoge.com", codeLen, time);
 		
-		System.out.println("now: " + now(30));
+		System.out.println("now:     " + (nowTiming(30) * 1000L));
+		System.out.println("current: " + System.currentTimeMillis());
+		System.out.println("out[0]: " + out[0]);
+		System.out.println("out[1]: " + out[1]);
 		
 		System.out.println("code1[0]: " + code1[0]);
 		System.out.println("code1[1]: " + code1[1]);
@@ -186,5 +267,5 @@ public final class GenerateSimpleTwoAuth {
 		System.out.println("code2[1]: " + code2[1]);
 		System.out.println("code2[2]: " + code2[2]);
 	}
-	*/
+	**/
 }
